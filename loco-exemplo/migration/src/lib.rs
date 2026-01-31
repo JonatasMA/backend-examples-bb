38f91b23458394e6860a4b72976c8d272ab12af6
JonatasMA <jonatasmacedo70@hotmail.com>
2026-01-31 11:12:59 -0300

    fix: adiciona rota e model event loco

--- loco-exemplo/migration/src/lib.rs
+++ loco-exemplo/migration/src/lib.rs
@@ -2,12 +2,14 @@
 #![allow(clippy::wildcard_imports)]
 pub use sea_orm_migration::prelude::*;
 
+mod m20260131_140819_events;
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
+            Box::new(m20260131_140819_events::Migration),
             // inject-above (do not remove this comment)
         ]
     }
